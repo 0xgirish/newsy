5ae99ff5972813c7762a4f6db2695f13c9dd797d
Girish Kumar <girish.k38@yahoo.com>
2018-03-19 18:24:30 +0530

    minor bug fix

--- signup/Signup_script.php
+++ signup/Signup_script.php
@@ -1,6 +1,6 @@
 <?php 
 require '../includes/common.php';
-$name = mysqli_real_escape_string($con,filter_input(INPUT_POST,'name'));
+$username = mysqli_real_escape_string($con,filter_input(INPUT_POST,'username'));
 $email_chk = filter_input(INPUT_POST,'email');
 $regex_email = "/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/";
 if(!preg_match($regex_email,$email_chk)){
@@ -8,29 +8,33 @@ if(!preg_match($regex_email,$email_chk)){
 }else{
     $email = mysqli_real_escape_string($con, $email_chk);
     $pass = filter_input(INPUT_POST,'pass');
-    $query = "SELECT count(*) FROM users WHERE email = '$email'";
+    $query = "SELECT count(*) as num FROM users WHERE email = '$email'";
     $email_exist = $con->query($query) or die(include 'wentWrong.php');
-    if($email_exist){
+    $result = $email_exist->fetch_assoc();
+    if($result['num'] > 0){
         header('location: signup.php?error=Email already exist');
-        $query = "SELECT count(*) FROM users WHERE username = '$username'";
+    }else{
+        $query = "SELECT count(*) as num FROM users WHERE username = '$username'";
         $username_exist = $con->query($query);
-        if($username_exist){
+        $result = $username_exist->fetch_assoc();
+        if($result['num'] > 0){
             header('location: signup.php?error=Username already exist');
         }else{
-            if(strlen($pass)<7){
+            if(strlen($pass)< 7){
                     header('location: signup.php?error=Password Requirement Failed');
             }else{
                 $password = mysqli_real_escape_string($con,md5($pass));
-                $username = mysqli_real_escape_string($con,filter_input(INPUT_POST,'username'));
+                $name = mysqli_real_escape_string($con,filter_input(INPUT_POST,'name'));
                 if(strlen($username)<5){
                     header('location: signup.php?error=Username Requirement Failed');
                 }else{
                     $insert_query = "INSERT INTO users(name,email, username, password) values('$name','$email','$username', '$password')";
-                    $insert_result = mysqli_query($con, $insert_query) or die(include 'wentWrong.php');
+                    $insert_result = mysqli_query($con, $insert_query);
+                    if(!($insert_result)) echo $con->error;
                     session_start();
                     $_SESSION['email']=$email;
                     $_SESSION['id'] = mysqli_insert_id($con);
-                    header('location: home.php');
+                    #header('location: home.php');
                 }
             }
         }
